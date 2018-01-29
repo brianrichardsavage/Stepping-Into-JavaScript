252e43efd0bcda89d282c8f6e2ad93347afbf605
brianrichardsavage <brian.richard.savage@gmail.com>
2018-01-28 23:19:40 -0500

    Added a for loop to the test.js file.

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 // JavaScript Document
-var age = 5;
 
-while (age < 10) {
-	console.log("Your age is less than 10");
-	age++;
+var links = document.getElementsByTagName("a");
+
+for (i = 1; i <= links.length; i++) {
+	console.log("this is the link number" + i);
 }
-	document.write("you are now over 10");
+	document.write("all links now looped");
 

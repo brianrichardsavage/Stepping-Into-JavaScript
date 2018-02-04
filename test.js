412b101a652144f8042bab9c1894970880b7fd94
brianrichardsavage <brian.richard.savage@gmail.com>
2018-02-04 16:06:26 -0500

    completed the functions - assignment 10

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
 // JavaScript Document
 
-var links = document.getElementsByTagName("a");
-
-for (i = 0; i <= links.length; i++) {
-	links[i].className = "link-" + i;
+function getAverage(a,b){
+  var average = (a + b) / 2;
+  console.log(average);
+  return average;
 }
+
+var myResult = getAverage(7,8,9);
+console.log("the avarage is " + myResult);
+
